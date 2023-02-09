3b1bbc1896f5227c3dfd76c914274f84096cba1f
Web Perfection Technology <surendersharma48@gmail.com>
2023-02-09 17:05:00 +0530

    Add files via upload

--- admin/js/custom.js
+++ admin/js/custom.js
@@ -1,1 +1,8 @@
-
+jQuery(".checkedpromocode").click(function(){
+ if(jQuery(this).is(":checked")) {
+   alert(jQuery(this).val());
+        jQuery(this).attr('checked');
+   }else{
+       jQuery(this).removeAttr('checked');
+   }
+});
